39e9332b89052457896b415cf7b5d3ecb02400e3
Dmitry Sazonov <sazon@nxt.ru>
2019-09-13 18:47:48 +0300

    catalog deleteing url functionality

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,9 +3,29 @@
 namespace App\Controller;
 
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use App\Service\TagService;
+use Doctrine\ORM\EntityManager;
+use App\Entity\Product;
 
 class AdminController extends BaseAdminController
 {
+//    public function __construct(
+//        TagService $tagService,
+//        EntityManager $em
+//    ) {
+//        $this->em = $em;
+//        $product = $this->em->getRepository('App:Product')->find(16);
+////var_dump($product->getId());
+////die();
+//        $this->tagService = $tagService;
+//        $similar = $this->tagService
+//            ->setEntity($product)
+//            ->setEntityType(Product::class)
+//            ->similars();
+//        print_r($similar);
+//        die();
+//    }
+
     public function repository($className)
     {
         return $this->getDoctrine()->getRepository($className);
