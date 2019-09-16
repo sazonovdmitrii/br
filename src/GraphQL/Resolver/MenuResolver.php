716cb7791038e8da105d020f3965b85b4f57186b
Dmitry Sazonov <sazon@nxt.ru>
2019-09-16 18:02:05 +0300

    different menus

---
+++ src/GraphQL/Resolver/MenuResolver.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\GraphQL\Resolver;
+
+use Doctrine\ORM\EntityManager;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+use Overblog\GraphQLBundle\Definition\Argument;
+use GraphQL\Error\UserError;
+
+class MenuResolver extends LocaleAlias
+{
+    private $em;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+
+    public function resolve(Argument $args)
+    {
+        $topMenu = $this->em->getRepository('App:Menu')
+            ->findOneBy(
+                ['name' => $args['name'] . '_' . $this->getLocale()]
+            );
+
+        $result = [];
+        if($topMenu && $topMenu->getMenu()) {
+            $menu = json_decode($topMenu->getMenu(), true);
+            $result = $menu[0]['children'];
+        }
+        return [
+            'data' => $result
+        ];
+    }
+
+    public static function getAliases()
+    {
+        return [
+            'resolve' => 'Menu'
+        ];
+    }
+}
