40b4b648d9ad683de78510517111fc82b6999b2b
Dmitry Sazonov <sazon@nxt.ru>
2019-10-09 15:02:05 +0300

    translations in admin

---
+++ src/Controller/Admin/TranslationController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Translation;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Response;
+use App\Service\JsonService;
+
+class TranslationController extends BaseAdminController
+{
+    private $entityManager;
+
+    private $jsonService;
+
+    public function __construct(
+        EntityManager $entityManager,
+        JsonService $jsonService
+    ) {
+        $this->entityManager = $entityManager;
+        $this->jsonService = $jsonService;
+    }
+
+    public function listTranslationAction()
+    {
+        $this->dispatch(EasyAdminEvents::PRE_LIST);
+
+        $fields = $this->entity['list']['fields'];
+        $paginator = $this->findAll($this->entity['class'], $this->request->query->get('page', 1), $this->entity['list']['max_results'], $this->request->query->get('sortField'), $this->request->query->get('sortDirection'), $this->entity['list']['dql_filter']);
+
+        $data = $this->getDoctrine()->getRepository(Translation::class)->findAll();
+
+        $this->dispatch(EasyAdminEvents::POST_LIST, array('paginator' => $paginator));
+
+        $parameters = array(
+            'paginator' => $paginator,
+            'fields' => $fields,
+            'data' => $data,
+            'delete_form_template' => $this->createDeleteForm($this->entity['name'], '__id__')->createView(),
+        );
+
+        return $this->executeDynamicMethod('render<EntityName>Template', array('list', 'admin/Translation/page.html.twig', $parameters));
+    }
+
+    public function createTranslationAction()
+    {
+        $requestData = $this->request->request;
+
+        $translation = new Translation();
+        $translation->setSource($requestData->get('source'));
+        $translation->setGoal($requestData->get('goal'));
+        $translation->setLanguage($requestData->get('language'));
+
+        $this->entityManager->persist($translation);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['response' => 200]);
+    }
+
+    protected function deleteTranslationAction()
+    {
+        $this->dispatch(EasyAdminEvents::PRE_DELETE);
+
+        $id = $this->request->query->get('id');
+        $form = $this->createDeleteForm($this->entity['name'], $id);
+        $form->handleRequest($this->request);
+
+        $easyadmin = $this->request->attributes->get('easyadmin');
+        $entity = $easyadmin['item'];
+
+        $this->dispatch(EasyAdminEvents::PRE_REMOVE, array('entity' => $entity));
+
+        $this->executeDynamicMethod('preRemove<EntityName>Entity', array($entity, true));
+
+        try {
+            $this->executeDynamicMethod('remove<EntityName>Entity', array($entity, $form));
+        } catch (ForeignKeyConstraintViolationException $e) {
+            throw new EntityRemoveException(array('entity_name' => $this->entity['name'], 'message' => $e->getMessage()));
+        }
+
+        $this->dispatch(EasyAdminEvents::POST_REMOVE, array('entity' => $entity));
+
+        $this->dispatch(EasyAdminEvents::POST_DELETE);
+
+        return $this->redirectToReferrer();
+    }
+
+    public function generateAction()
+    {
+        $locales = $this->getParameter('a2lix_translation_form.locales');
+        foreach($locales as $locale) {
+
+            $languages = $this->getDoctrine()
+                ->getRepository(Translation::class)
+                ->findBy(['language' => $locale]);
+
+            $this->jsonService
+                ->setData($languages)
+                ->setLocale($locale)
+                ->generate();
+        }
+        return $this->redirect($this->generateUrl('easyadmin', array('action' => 'list', 'entity' => $this->entity['name'])));
+    }
+}
---
+++ src/Service/JsonService.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Service;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManager;
+
+class JsonService extends AbstractController
+{
+    private $entityManager;
+
+    private $data;
+
+    private $dir;
+
+    private $locale;
+
+    private $localeDir;
+
+    public function __construct($localeDir, EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->localeDir = $localeDir;
+    }
+
+    public function setData(array $data)
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function setLocale(string $locale)
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    public function generate()
+    {
+        if(!$this->getData()) {
+            return false;
+        }
+
+        $data = [];
+        foreach($this->getData() as $item) {
+            $data[$item->getSource()] = $item->getGoal();
+        }
+
+        if(!file_exists($this->localeDir)) {
+            $this->_createDir($this->localeDir);
+        }
+
+        file_put_contents($this->localeDir . $this->getLocale() . '.json', json_encode($data));
+    }
+
+    private function _createDir($path)
+    {
+        return mkdir($path, 0777, true);
+    }
+}
