ef9e21c752f8ca3855ca2c958239a4242f4c384d
Alexey Chernyshev <er1x4ever@gmail.com>
2019-10-15 13:19:38 +0300

    15908

--- front/src/routes/Landing/Sunglasses.js
+++ front/src/routes/Landing/Sunglasses.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Link as LinkRoute } from 'react-router-dom';
+// import { Link as LinkRoute } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
 import { useLangLinks } from 'hooks';
 
-import Link from 'components/Link';
-import Collections from 'components/Collections';
+// import Collections from 'components/Collections';
 import Container from 'components/Container';
 import Button from 'components/Button';
-import VideoSection from 'components/VideoSection';
+// import VideoSection from 'components/VideoSection';
 import HomeTry from 'components/HomeTry';
 import Lenses from 'components/Lenses';
 
 import styles from './styles.css';
 import heroImage from './images/sunglasses-hero.jpg';
 import homeTryImage from './images/sunglasses-hometry.jpg';
-import lensesImage from './images/sunglasses-lenses.jpg';
-import videoPosterImage from './images/videposter.jpg';
+import lensesImage from './images/eyeglasses-lenses.jpg';
+// import videoPosterImage from './images/videposter.jpg';
 
 const Landing = () => {
     const [manUrl, womanUrl] = useLangLinks([
@@ -56,42 +55,44 @@ const Landing = () => {
                     text={<FormattedMessage id="sunglasses_home_try_text" />}
                     action={
                         <Button to="" kind="simple" bold>
-                            <FormattedMessage id="get_started" />
+                            <FormattedMessage id="sunglasses_home_try_button" />
                         </Button>
                     }
                 />
             </section>
-            <section className={styles.section}>
-                <VideoSection
-                    title={<FormattedMessage id="video_title" />}
-                    poster={videoPosterImage}
-                    video="https://player.vimeo.com/external/246989570.hd.mp4?s=276721bec238e24770d1c7e8f7678ab224480188&amp;profile_id=174"
-                    actions={
-                        <LinkRoute to="/how-our-glasses-are-made" className={styles.button}>
-                            <FormattedMessage id="video_action" />
-                        </LinkRoute>
-                    }
-                />
-            </section>
+            {/*
+                <section className={styles.section}>
+                    <VideoSection
+                        title={<FormattedMessage id="video_title" />}
+                        poster={videoPosterImage}
+                        video="https://player.vimeo.com/external/246989570.hd.mp4?s=276721bec238e24770d1c7e8f7678ab224480188&amp;profile_id=174"
+                        actions={
+                            <LinkRoute to="/how-our-glasses-are-made" className={styles.button}>
+                                <FormattedMessage id="video_action" />
+                            </LinkRoute>
+                        }
+                    />
+                </section>
+            */}
             <section className={styles.section}>
                 <Lenses
                     image={<img src={lensesImage} alt="" />}
-                    title={<FormattedMessage id="sunglasses_lenses_title" />}
-                    text={<FormattedMessage id="sunglasses_lenses_text" />}
+                    title={<FormattedMessage id="eyeglasses_lenses_title" />}
+                    text={<FormattedMessage id="eyeglasses_lenses_text" />}
                     items={[
                         {
-                            label: 'Prescription types',
-                            value: 'Single-vision, progressives, readers, non-prescription',
+                            label: <FormattedMessage id="prescription_types" />,
+                            value: <FormattedMessage id="prescription_types_text" />,
                         },
                     ]}
-                    actions={
-                        <Button to="/lenses" kind="simple" size="large" bold>
+                    action={
+                        <Button to="/info/lenses" kind="simple" size="large" bold>
                             <FormattedMessage id="find_your_lenses" />
                         </Button>
                     }
                 />
             </section>
-            <Collections />
+            {/* <Collections /> */}
         </Container>
     );
 };
