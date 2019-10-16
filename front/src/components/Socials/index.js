b62f16273dbdc0900aeb4739ab42970e84004f00
Alexey Chernyshev <er1x4ever@gmail.com>
2019-10-16 14:33:38 +0300

    15879

--- front/src/components/Socials/index.js
+++ front/src/components/Socials/index.js
@@ -1,53 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useQuery } from '@apollo/react-hooks';
 import classnames from 'classnames/bind';
-
-import { GET_SOCIALS } from 'query';
 
 import styles from './styles.css';
 
-import instagramIcon from './icons/instagram.svg';
-import youtubeIcon from './icons/youtube.svg';
-import twitterIcon from './icons/twitter.svg';
 import facebookIcon from './icons/facebook.svg';
+import instagramIcon from './icons/instagram.svg';
+import okIcon from './icons/ok.svg';
+import vkIcon from './icons/vk.svg';
 
 const cx = classnames.bind(styles);
 
-const Socials = ({ className }) => {
-    // const { loading, error, data: { socials = [] } = {} } = useQuery(GET_SOCIALS);
-    const socials = [
-        {
-            name: 'facebook',
-            url: '/',
-            icon: facebookIcon,
-        },
-        {
-            name: 'twitter',
-            url: '/',
-            icon: twitterIcon,
-        },
-        {
-            name: 'youtube',
-            url: '/',
-            icon: youtubeIcon,
-        },
-        {
-            name: 'instagram',
-            url: '/',
-            icon: instagramIcon,
-        },
-    ];
+const socials = [
+    {
+        name: 'vk',
+        url: 'https://www.facebook.com/Brillenhof-100867174671198/',
+        icon: vkIcon,
+    },
+    {
+        name: 'facebook',
+        url: 'https://www.facebook.com/Brillenhof-100867174671198/',
+        icon: facebookIcon,
+    },
+    {
+        name: 'instagram',
+        url: 'https://www.instagram.com/brillenhof/',
+        icon: instagramIcon,
+    },
+    {
+        name: 'ok',
+        url: 'https://ok.ru/group/52994139816095 ',
+        icon: okIcon,
+    },
+];
 
+const Socials = ({ className }) => {
     const rootClassName = cx(styles.root, className);
 
     if (!socials.length) return null;
 
     return (
         <ul className={rootClassName}>
-            {socials.map(({ url, name, icon: Icon }, index) => (
-                <li key={index} className={styles.item}>
-                    <a href={url} target="_blank" className={styles.link} rel="noopener">
+            {socials.map(({ url, name, icon: Icon }) => (
+                <li key={name} className={styles.item}>
+                    <a href={url} target="_blank" className={styles.link} rel="noopener noreferrer">
                         <Icon className={styles.icon} />
                     </a>
                 </li>
@@ -56,8 +52,12 @@ const Socials = ({ className }) => {
     );
 };
 
-Socials.defaultProps = {};
+Socials.defaultProps = {
+    className: null,
+};
 
-Socials.propTypes = {};
+Socials.propTypes = {
+    className: PropTypes.string,
+};
 
 export default Socials;
