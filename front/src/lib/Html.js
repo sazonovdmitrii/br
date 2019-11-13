b2d1365a206a6ccb6b1116f30d83cfb1c4ac7292
Alexey Chernyshev <er1x4ever@gmail.com>
2019-11-13 12:46:32 +0300

    16053

--- front/src/lib/Html.js
+++ front/src/lib/Html.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const GTM = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PV7HJ4Z');`;
 const LIVETEX = `
     window['l'+'i'+'ve'+'Te'+'x'] = true,
-    window['liv'+'e'+'Tex'+'ID'] = 163612,
+    window['liv'+'e'+'Tex'+'ID'] = 163611,
     window['li'+'veTex_'+'obje'+'ct'] = true;
     (function() {
         var t = document['create'+'El'+'em'+'ent']('script');
