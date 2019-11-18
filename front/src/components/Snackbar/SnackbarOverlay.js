938146bb11ce9700d5cde1d42f84b8761e7fa603
Alexey Chernyshev <er1x4ever@gmail.com>
2019-11-18 13:47:31 +0300

    fix ssr

--- front/src/components/Snackbar/SnackbarOverlay.js
+++ front/src/components/Snackbar/SnackbarOverlay.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
-import classnames from 'classnames/bind';
+
+import { isBrowser } from 'utils';
 
 import styles from './styles.css';
 
 const SnackbarOverlay = ({ children }) => {
-    const domNode = document.body;
+    if (isBrowser) {
+        const domNode = document.body;
 
-    if (typeof document !== 'undefined' && domNode) {
-        return createPortal(<div className={styles.overlay}>{children}</div>, domNode);
+        if (domNode) {
+            return createPortal(<div className={styles.overlay}>{children}</div>, domNode);
+        }
     }
+
     return null;
+};
+
+SnackbarOverlay.propTypes = {
+    children: PropTypes.node.isRequired,
 };
 
 export default SnackbarOverlay;
