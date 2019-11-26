f4ef37c345f11ba4d9d204ed3bd4ec2e3d8c6302
Alexey Chernyshev <er1x4ever@gmail.com>
2019-11-26 17:24:27 +0300

    16077

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import { FormattedMessage } from 'react-intl';
+import { useLocation } from 'react-router';
 
 import FooterMenu from 'components/FooterMenu';
 import Socials from 'components/Socials';
@@ -14,43 +15,56 @@ import styles from './styles.css';
 
 const cx = classnames.bind(styles);
 
-const Footer = ({ lang }) => (
-    <footer id="footer" className={styles.root}>
-        <section className={styles.top}>
-            <div className={styles.row}>
-                <FooterMenu lang={lang} className={styles.menu} />
-                <Help className={styles.help} />
-            </div>
-        </section>
-        <section className={styles.bottom}>
-            <div className={styles.countries}>
-                <ul>
-                    {LANGS.map(({ value, default: defaultLang, native }) => {
-                        const countryLink = cx(styles.countryLink, { active: value === lang });
+const Footer = ({ lang }) => {
+    const { pathname } = useLocation();
 
-                        return (
-                            <li key={native} className={styles.countryItem}>
-                                <a href={`/${!defaultLang ? value : ''}`} className={countryLink}>
-                                    {native}
-                                </a>
-                            </li>
-                        );
-                    })}
-                </ul>
-                <div className={styles.socials}>
-                    <Socials />
+    return (
+        <footer id="footer" className={styles.root}>
+            <section className={styles.top}>
+                <div className={styles.row}>
+                    <FooterMenu lang={lang} className={styles.menu} />
+                    <Help className={styles.help} />
                 </div>
-            </div>
-            <CopyrightMenu lang={lang} />
-        </section>
-        <section className={styles.copyright}>
-            <FormattedMessage id="c_footer_copyright" values={{ br: <br /> }} />
-        </section>
-        <section className={styles.disclamer}>
-            <FormattedMessage id="c_footer_disclamer" />
-        </section>
-    </footer>
-);
+            </section>
+            <section className={styles.bottom}>
+                <div className={styles.langs}>
+                    <ul>
+                        {LANGS.map(({ value, default: defaultLang, native }) => {
+                            const isActive = value === lang;
+                            const langLink = cx(styles.langLink, { active: isActive });
+                            const href = `/${defaultLang ? '' : `${value}`}${pathname.replace(
+                                /\/\w{2}\//,
+                                ''
+                            )}`;
+
+                            return (
+                                <li key={native} className={styles.langItem}>
+                                    {isActive ? (
+                                        <span className={langLink}>{native}</span>
+                                    ) : (
+                                        <a href={href} className={langLink}>
+                                            {native}
+                                        </a>
+                                    )}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <div className={styles.socials}>
+                        <Socials />
+                    </div>
+                </div>
+                <CopyrightMenu lang={lang} />
+            </section>
+            <section className={styles.copyright}>
+                <FormattedMessage id="c_footer_copyright" values={{ br: <br /> }} />
+            </section>
+            <section className={styles.disclamer}>
+                <FormattedMessage id="c_footer_disclamer" />
+            </section>
+        </footer>
+    );
+};
 
 Footer.propTypes = {
     lang: PropTypes.string.isRequired,
