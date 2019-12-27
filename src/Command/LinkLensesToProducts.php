e6e712deac49ea395b5a5888af8e86b697383025
Dmitry Sazonov <sazon@nxt.ru>
2019-12-27 11:39:23 +0300

    link lenses to products

---
+++ src/Command/LinkLensesToProducts.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Command;
+
+use App\Repository\LenseRepository;
+use App\Repository\ProductRepository;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Doctrine\ORM\EntityManager;
+
+class LinkLensesToProducts extends ContainerAwareCommand
+{
+    protected static $defaultName = 'products:linklenses';
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+    /**
+     * @var LenseRepository
+     */
+    private $lenseRepository;
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Link Lenses To Products')
+            ->setHelp('');
+    }
+
+    public function __construct(
+        ProductRepository $productRepository,
+        EntityManager $entityManager,
+        LenseRepository $lenseRepository
+    ) {
+        $this->productRepository = $productRepository;
+        $this->entityManager = $entityManager;
+        $this->lenseRepository = $lenseRepository;
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $lenses = $this->lenseRepository->findAll();
+
+        foreach($this->productRepository->findAll() as $product) {
+            foreach($lenses as $lense) {
+                $product->addLense($lense);
+            }
+
+            $this->entityManager->persist($product);
+        }
+        
+        $this->entityManager->flush();
+
+        $output->writeln(['Link has done']);
+    }
+}
