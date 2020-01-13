dfd8e22108a21586ec21be52e27f548785d4900e
Alexey Chernyshev <er1x4ever@gmail.com>
2020-01-13 15:10:00 +0300

    add seo head

---
+++ front/src/components/SeoHead/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useLocation } from 'react-router';
+
+import { useLang } from 'hooks';
+import SEO from 'globalMeta';
+
+const SeoHead = props => {
+    const { type, image } = props;
+    const { pathname: url } = useLocation();
+    const { title, description, keywords, ogType = 'website' } = SEO[type](props);
+    const { lang } = useLang();
+
+    return (
+        <Helmet>
+            {title && <title>{title}</title>}
+            {description && <meta name="description" content={description} />}
+            {keywords && <meta name="keywords" content={keywords} />}
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:type" content={ogType} />
+            <meta property="og:url" content={`${SEO.url}${url}`} />
+            {image && <meta property="og:image" content={`${SEO.url}${image}`} />}
+            <meta property="og:site_name" content={SEO.fullSiteName} />
+            <meta property="og:locale" content={lang} />
+        </Helmet>
+    );
+};
+
+export default SeoHead;
