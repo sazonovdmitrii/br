b73e8d290e4352b8fc21f9629164c8c618e89dc2
Dmitry Sazonov <sazon@nxt.ru>
2020-01-15 21:13:38 +0300

    ci test

--- src/Command/FeedData/FeedFactory.php
+++ src/Command/FeedData/FeedFactory.php
@@ -6,16 +6,18 @@ class FeedFactory
 {
     private $type;
     private $configService;
+    private $envService;
 
-    public function __construct($type, $configService)
+    public function __construct($type, $configService, $envService)
     {
         $this->type = $type;
         $this->configService = $configService;
+        $this->envService = $envService;
     }
 
     public function getDataProvider()
     {
         $class = __NAMESPACE__ . '\\' . ucfirst($this->type);
-        return new $class($this->configService);
+        return new $class($this->configService, $this->envService);
     }
 }
