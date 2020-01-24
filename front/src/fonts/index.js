d3cb595029c67ed4cb0b6d738b692601b8a2b669
Alexey Chernyshev <er1x4ever@gmail.com>
2020-01-24 12:18:11 +0300

    clean up

--- front/src/fonts/index.js
+++ front/src/fonts/index.js
@@ -1,5 +1,3 @@
 // https://fonts.google.com/specimen/Literata
 import './literata';
-
 import './proxima-nova';
-// import './utopia-std';
--- front/src/routes/Personal.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import UserForm from 'components/UserForm';
-
-export default () => <UserForm type="personal" />;
