f1f895bcacb2dde8eed30b6420fa47eaf625ee10
Alexey Chernyshev <er1x4ever@gmail.com>
2020-02-14 13:55:56 +0300

    16584

--- front/src/routes/Basket/Sidebar/index.js
+++ front/src/routes/Basket/Sidebar/index.js
@@ -6,7 +6,7 @@ import styles from './styles.css';
 
 const cx = classnames.bind(styles);
 
-export default ({ className, messages = [], actions, pricing = [] }) => {
+export default ({ className, messages = [], coupon, actions, pricing = [] }) => {
     const rootClassName = cx(styles.root, className);
 
     return (
@@ -20,19 +20,22 @@ export default ({ className, messages = [], actions, pricing = [] }) => {
                     ))}
                 </ul>
             ) : null}
-            <div className={styles.pricing}>
-                {pricing.map(({ name, value }) => (
-                    <p key={name} className={styles.pricingItem}>
-                        <FormattedMessage id={name} />
-                        <span className={styles.pricingValue}>
-                            {value ? (
-                                <FormattedMessage id="currency" values={{ price: value }} />
-                            ) : (
-                                <FormattedMessage id="free" />
-                            )}
-                        </span>
-                    </p>
-                ))}
+            <div className={styles.reverse}>
+                <div className={styles.pricing}>
+                    {pricing.map(({ name, value }) => (
+                        <p key={name} className={styles.pricingItem}>
+                            <FormattedMessage id={name} />
+                            <span className={styles.pricingValue}>
+                                {value ? (
+                                    <FormattedMessage id="currency" values={{ price: value }} />
+                                ) : (
+                                    <FormattedMessage id="free" />
+                                )}
+                            </span>
+                        </p>
+                    ))}
+                </div>
+                {coupon && <div className={styles.coupon}>{coupon}</div>}
             </div>
             <div className={styles.actions}>
                 <div className={styles.actionsInner}>{actions}</div>
