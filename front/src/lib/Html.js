23975a85d84cf052184548c77a42a454a0a89a5a
Alexey Chernyshev <er1x4ever@gmail.com>
2020-02-18 15:58:44 +0300

    yandex metrika

--- front/src/lib/Html.js
+++ front/src/lib/Html.js
@@ -4,6 +4,17 @@ const isProd = process.env.NODE_ENV === 'production';
 const GTM = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NLL4WS');`;
 // Anti-Flicker Snippet Google Optimize
 const AFSGO = `(function(a,s,y,n,c,h,i){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;})(window,document.documentElement,'async-hide','dataLayer',2000,{'GTM-PV7HJ4Z':true});`;
+const YANDEX_METRIKA = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ym(55754116, "init", {
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true,
+    webvisor:true,
+    trackHash:true,
+    ecommerce:"dataLayer"
+});`;
 
 export default ({ bundle, helmet, html, window = {} }) => (
     <html {...helmet.htmlAttributes.toString()}>
@@ -23,6 +34,11 @@ export default ({ bundle, helmet, html, window = {} }) => (
                             __html: GTM,
                         }}
                     />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: YANDEX_METRIKA,
+                        }}
+                    />
                     {/* 16103 */}
                     <script
                         dangerouslySetInnerHTML={{
@@ -38,14 +54,25 @@ export default ({ bundle, helmet, html, window = {} }) => (
         </head>
         <body>
             {isProd && (
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-NLL4WS"
-                        height="0"
-                        width="0"
-                        style={{ display: 'none', visibility: 'hidden' }}
-                    />
-                </noscript>
+                <>
+                    <noscript>
+                        <iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-NLL4WS"
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        />
+                    </noscript>
+                    <noscript>
+                        <div>
+                            <img
+                                src="https://mc.yandex.ru/watch/55754116"
+                                style="position:absolute; left:-9999px;"
+                                alt=""
+                            />
+                        </div>
+                    </noscript>
+                </>
             )}
             <div id="root" dangerouslySetInnerHTML={{ __html: html }} />
             <script
