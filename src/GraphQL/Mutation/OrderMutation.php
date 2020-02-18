03dda7e451058e54f88ef4ac8ec552fd1b16b5d5
Dmitry Sazonov <sazon@nxt.ru>
2020-02-18 17:24:17 +0300

    lenses

--- src/GraphQL/Mutation/OrderMutation.php
+++ src/GraphQL/Mutation/OrderMutation.php
@@ -136,7 +136,9 @@ class OrderMutation extends AuthMutation
             if($productItem) {
                 $orderItem->setItem($productItem);
             }
-            $orderItem->setLenses(json_encode($basketItem['lenses']));
+            if(isset($basketItem['lenses'])) {
+                $orderItem->setLenses(json_encode($basketItem['lenses']));
+            }
 
             $this->manager->persist($orderItem);
             $this->manager->flush();
