402357e8ba55e04f4c6addb6508b3b93aeaecbcb
Alexey Chernyshev <er1x4ever@gmail.com>
2020-02-27 14:08:08 +0300

    fix trouble with basket cache after addToBasket

--- front/src/mutations/addToBasket.js
+++ front/src/mutations/addToBasket.js
@@ -1,14 +1,13 @@
 import gql from 'graphql-tag';
 
-import { BasketProducts } from 'fragments';
-
 const ADD_TO_BASKET = gql`
     mutation addBasket($input: AddBasketInput!) {
         addBasket(input: $input) {
-            ...BasketProducts
+            products {
+                name
+            }
         }
     }
-    ${BasketProducts}
 `;
 
 export default ADD_TO_BASKET;
