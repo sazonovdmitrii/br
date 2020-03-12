4472d431af9453b2f140ba318f9856121161f90f
Dmitry Sazonov <sazon@nxt.ru>
2020-03-12 14:36:22 +0300

    if liink

--- src/Command/FeedData/Facebook.php
+++ src/Command/FeedData/Facebook.php
@@ -7,7 +7,9 @@ class Facebook extends FeedDataAbstract
     public function getData()
     {
         $data = parent::getData();
-        $data['link'] .= '?utm_source=facebook&utm_medium=cpc&utm_campaign=remarketing_facebook';
+        if(isset($data['link'])) {
+            $data['link'] .= '?utm_source=facebook&utm_medium=cpc&utm_campaign=remarketing_facebook';
+        }
         return $data;
     }
 }
